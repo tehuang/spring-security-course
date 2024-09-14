59c976589e8f0cae76e100e971cc12f55963e49c
Teresa Huang <teresayhhuang@gmail.com>
2024-09-14 10:31:48 -0400

    Encrypt passwords

--- lesson-2-5/src/main/java/com/kucw/security/controller/MemberController.java
+++ lesson-2-5/src/main/java/com/kucw/security/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.kucw.security.controller;
 import com.kucw.security.dao.MemberDao;
 import com.kucw.security.model.Member;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +13,17 @@ public class MemberController {
     @Autowired
     private MemberDao memberDao;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @PostMapping("/register")
     public Member register(@RequestBody Member member){
         //Ignore parameters checking
+
+        //Hashed Password
+        String hashedPassword = passwordEncoder.encode(member.getPassword());
+        member.setPassword(hashedPassword);
+
         //Insert a member into database
         Integer memberId = memberDao.createMember(member);
         return memberDao.getMemberById(memberId);
