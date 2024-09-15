014a647d22290503c5e3a6c7b50a5a75f8845a59
Teresa Huang <teresayhhuang@gmail.com>
2024-09-15 13:43:18 -0400

    Retrieve authentication information in the API

--- lesson-2-5/src/main/java/com/kucw/security/controller/MyController.java
+++ lesson-2-5/src/main/java/com/kucw/security/controller/MyController.java
@@ -1,14 +1,22 @@
 package com.kucw.security.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
 
 @RestController
 public class MyController {
 
     @GetMapping("/hello")
-    public String hello() {
-        return "Hello!";
+    public String hello(Authentication authentication)
+    {   //Get user's account
+        String username=authentication.getName();
+        //Get user's authorities
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        return "Hello! "+username+" !";
     }
 
     @GetMapping("/welcome")
