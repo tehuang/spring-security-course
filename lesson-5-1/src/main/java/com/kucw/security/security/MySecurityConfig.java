de6404a15882833eee181995dfc4961b90c54849
Teresa Huang <teresayhhuang@gmail.com>
2024-10-04 13:59:18 -0400

    Set up CORS settings in Spring Security

---
+++ lesson-5-1/src/main/java/com/kucw/security/security/MySecurityConfig.java
@@ -0,0 +1,68 @@
+package com.kucw.security.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+public class MySecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails userTest1 = User
+                .withUsername("test1")
+                .password("{noop}111")
+                .roles("ADMIN", "USER")
+                .build();
+
+        UserDetails userTest2 = User
+                .withUsername("test2")
+                .password("{noop}222")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(userTest1, userTest2);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        return http
+                .csrf(csrf -> csrf.disable())
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults())
+
+                .authorizeHttpRequests(request -> request
+                        .anyRequest().authenticated()
+                )
+                .cors(cors -> cors
+                        .configurationSource(createCorsConfig())
+                )
+                .build();
+    }
+
+    private CorsConfigurationSource createCorsConfig(){
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("*")); //後端允許的請求來源 e.g. https://example.com
+        config.setAllowedHeaders(List.of("*")); //後端允許的request header有哪些
+        config.setAllowedMethods(List.of("*")); //後端允許的http method有哪些 e.g. "GET", "POST"
+        //config.setAllowCredentials(true);  //後端是否允許前端帶上cookie
+        config.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**",config);
+        return source;
+    }
+}
