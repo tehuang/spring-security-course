5662e8570a75f8df61c58c543984758691b70410
Teresa Huang <teresayhhuang@gmail.com>
2024-10-04 14:20:38 -0400

    Set up CORS settings in Spring Boot (@CrossOrigin)

--- lesson-5-1/src/main/java/com/kucw/security/controller/HelloController.java
+++ lesson-5-1/src/main/java/com/kucw/security/controller/HelloController.java
@@ -1,8 +1,10 @@
 package com.kucw.security.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*")  //equals to setAllowedOrigins()
 @RestController
 public class HelloController {
 
