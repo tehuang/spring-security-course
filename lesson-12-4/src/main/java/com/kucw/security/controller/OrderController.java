728ed76c7b5ccc4ba503b79bfe3aa70b802b9b7e
Teresa Huang <teresayhhuang@gmail.com>
2024-10-26 20:12:47 -0400

    Setup Resource Server

---
+++ lesson-12-4/src/main/java/com/kucw/security/controller/OrderController.java
@@ -0,0 +1,15 @@
+package com.kucw.security.controller;
+
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrderController {
+
+    @GetMapping("/order")
+    public String order(@AuthenticationPrincipal Jwt jwt) {
+        jwt.getIssuedAt();
+        return "返回訂單數據";
+    }
+}
